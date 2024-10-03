e4be974f0352d05e2843e17549ecb0502a0a8a07
dilshod505 <vohidjonov505@gmail.com>
2024-10-04 00:16:02 +0500

    third-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import './App.css'
-import Header from "./Header/page.tsx";
+import MainHeader from "./components/header.tsx";
 
 function App() {
 
-  return (
-    <>
-        <div className={'bg-black text-white h-screen'}>
-            {/*<video autoPlay muted loop className={'w-full h-full object-cover'}>*/}
-            {/*    <source src="/48420-453832153.mp4" type="video/mp4"/>*/}
-            {/*</video>*/}
-            <Header/>
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div className={'text-white h-screen'}>
+                <video
+                    autoPlay
+                    muted
+                    loop
+                    id="myVideo"
+                    className="absolute w-[100%]  bg-cover bg-center object-cover min-h-[100vh] -z-10"
+                >
+                    <source src="/48420-453832153.mp4" type="video/mp4"/>
+                </video>
+                <MainHeader/>
+            </div>
+        </>
+    )
 }
 
 export default App
